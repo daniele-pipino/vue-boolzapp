eb93489d5e682bd075ae20bebfecd64706ff4fdc
daniele-pipino <Danielemacpipino@icloud.com>
2021-08-27 16:42:12 +0200

    message visualization

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ const app = new Vue({
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'received',
+                        status: 'sent',
                     },
                 ],
             },
